ea6d150e0f596eb9fa130c71d4913cd93005e8af
chriseadie <caeadie@hotmail.com>
2022-06-08 12:06:02 +0200

    initial c

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      New pin react application
+    </div>
+  );
+}
+
+export default App;
